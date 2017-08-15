2534a6e7730b86c20922b2ac2510b90efaafb081
Eko Teguh Widodo <dekteguh@gmail.com>
2017-08-15 10:35:05 +0700

    Implement state; handle onchange, and onsubmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css';
+
 class App extends React.Component {
+
+    constructor() {
+        super();
+        
+        this.state = {
+            newDate: '',
+            birthday: '1989-03-10'
+        }
+
+        this.handleChangeDate = this.handleChangeDate.bind(this)
+        this.handleClickBirthday = this.handleClickBirthday.bind(this)
+    }
+
+    handleChangeDate(e) {
+        this.setState({
+            newDate: e.target.value,
+        });
+    }
+
+    handleClickBirthday(e) {
+        this.setState({
+            birthday: this.state.newDate
+        })
+    }
+
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div className="App">
+                <h2>Masukkan Tanggal Lahir</h2>
+                <Form inline>
+                    <FormControl
+                        type="date"
+                        onChange={this.handleChangeDate} ></FormControl>
+                    {' '}
+                    <Button onClick={this.handleClickBirthday}>
+                        Hitung
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
